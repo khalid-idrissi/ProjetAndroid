cc8b7e3b645ed36badbc278e1d990dd10e08d533
khalid-idrissi <khalid-idrissi@hotmail.fr>
2023-07-26 18:32:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-import pynetbox
-print('test from git hub')
-tnetbox = 'jhfdsghfgsdhgfhsdghfgsdhgfhsdghfg'
-nb = pynetbox.api(
-            url='https://netbox.cbc-rc.ca/',
-            token=tnetbox
-        )
-nb.http_session.verify = False
-devices = nb.dcim.devices.get(name='MTLKJHIU76')
-print(devices)
+# import pynetbox
+# print('test from git hub')
+# tnetbox = 'jhfdsghfgsdhgfhsdghfgsdhgfhsdghfg'
+# nb = pynetbox.api(
+#             url='https://netbox.cbc-rc.ca/',
+#             token=tnetbox
+#         )
+# nb.http_session.verify = False
+# devices = nb.dcim.devices.get(name='MTLKJHIU76')
+# print(devices)
+
+print('test jenkins')
 
